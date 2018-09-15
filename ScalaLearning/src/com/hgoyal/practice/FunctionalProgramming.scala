dc0e432403ab84629ef5b488df5409d14129c2e9
Hershit <Nitika@192.168.0.100>
2018-09-15 15:16:24 +0530

    fixed and added comments

--- ScalaLearning/src/com/hgoyal/practice/FunctionalProgramming.scala
+++ ScalaLearning/src/com/hgoyal/practice/FunctionalProgramming.scala
@@ -1,24 +1,26 @@
+package com.hgoyal.practice
+
 object FunctionalProgramming
 {
   def main(args: Array[String]): Unit = {
     
-    print(sum(id, 1, 10))
+    println(sum(id, 1, 10)) // sum of numbers from 1 to 10
+    println(sum(square, 1, 10)) // sum of squares of nos from 1 to 10
+    println(sum(cube, 1, 10)) // sum of cubes of nos from 1 to 10
     
-    def sum(func:Int => Int, st:Int, end:Int)
+    def sum(func:Int => Int, st:Int, end:Int):Int =
     {  
       var total = 0
       for (index <- st to end)
       {
-        total += (index)
+        total += func(index)
       }
       total
     }
-  
-    def cube(i:Int) = i*i*i;
-    def square(i:Int) = i*i;
-    def id(i:Int) = i;
-    
-    
   }
+  
+  def cube(i:Int) = i*i*i;
+  def square(i:Int) = i*i;
+  def id(i:Int) = i;
   
 }
